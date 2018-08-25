8ec0414f29d2a2f192111b2434bc2d968aed6391
Mateusz Nurek <nurek.mateusz@gmail.com>
2018-08-25 22:05:21 +0200

    Add empty BoxComponent, AddCardComponent, LearningComponent and add routing between components

--- client/flashcards/src/app/app.module.ts
+++ client/flashcards/src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { AddCardComponent } from './add-card/add-card.component';
+import { LearningComponent } from './learning/learning.component';
+
+const appRoutes: Routes = [
+  { path: 'box', component: BoxComponent },
+  { path: 'add', component: AddCardComponent },
+  { path: 'learn', component: LearningComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    AddCardComponent,
+    LearningComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
