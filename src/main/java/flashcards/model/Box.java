f0c9bd1069a42ec28879936a914df745d0358904
Mateusz Nurek <nurek.mateusz@gmail.com>
2018-08-25 17:18:30 +0200

    Add getting box by id

--- src/main/java/flashcards/model/Box.java
+++ src/main/java/flashcards/model/Box.java
@@ -1,7 +1,22 @@
 package flashcards.model;
 
+import java.util.Arrays;
+
 public enum Box {
 
-    ONE, TWO, THREE, FOUR, FIVE, LEARNED
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), LEARNED(6);
+
+    private int boxId;
+
+    Box(int boxId) {
+        this.boxId = boxId;
+    }
+
+    public static Box getBoxById(int boxId) {
+        return Arrays.stream(values())
+                .filter(b -> b.boxId == boxId)
+                .findFirst()
+                .orElse(Box.ONE);
+    }
 
 }
