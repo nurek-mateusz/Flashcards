75d7df14e2245d857975e2e25c8e2e1e60e853f6
Mateusz Nurek <nurek.mateusz@gmail.com>
2018-08-25 17:21:09 +0200

    Add card controller

---
+++ src/main/java/flashcards/controller/CardController.java
@@ -0,0 +1,34 @@
+package flashcards.controller;
+
+import flashcards.model.Box;
+import flashcards.model.Card;
+import flashcards.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CardController {
+
+    @Autowired
+    private CardService cardService;
+
+    @GetMapping(value = "/box/{boxId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Card> getAllCardsInBox(@PathVariable int boxId) {
+        Box selectedBox = Box.getBoxById(boxId);
+        return cardService.getAllCardsFromBox(selectedBox);
+    }
+
+    @PutMapping(value = "/card/{cardId}/{answer}")
+    public void saveUserAnswer(@PathVariable long cardId, @PathVariable boolean answer) {
+        cardService.moveCardToNewBox(cardId, answer);
+    }
+
+    @PostMapping(value = "/card", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addNewCard(@RequestBody Card card) {
+        cardService.saveCard(card);
+    }
+
+}
